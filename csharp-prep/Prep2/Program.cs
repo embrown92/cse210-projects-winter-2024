16d0f1cc2004a19f7259fd81b661f26c3ba2065d
Eda Mae Brown <eda_awe92@yahoo.com.ph>
2024-01-10 01:09:50 -0500

    Version 1

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade percentage. ");
+        string studentInput = Console.ReadLine();
+        int gradePercentage = int.Parse(studentInput);
+
+        if (gradePercentage >  90)
+        {
+            Console.WriteLine("You got an A!");
+        }
+        else if (gradePercentage > 80)
+        {
+            Console.WriteLine("You got a B!");
+        }
+        else if (gradePercentage > 70)
+        {
+            Console.WriteLine("You got a C.");
+        }
+        else if (gradePercentage > 60)
+        {
+            Console.WriteLine("You got a D.");
+        }
+        else
+        {
+            Console.WriteLine("You got an F.");
+        }
+
+        if (gradePercentage > 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately, you failed. Please try again next time.");
+        }
     }
 }
