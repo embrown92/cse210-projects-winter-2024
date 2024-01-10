993b2cf7f2586a309dea4f78cc17a56578955527
Eda Mae Brown <eda_awe92@yahoo.com.ph>
2024-01-10 01:32:45 -0500

    Finished Prep2 stretch challenge

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,25 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Ask the user for their grade percentage
         Console.Write("Please enter your grade percentage. ");
         string studentInput = Console.ReadLine();
         int gradePercentage = int.Parse(studentInput);
 
         string letter = "";
 
-        if (gradePercentage >  90)
+        // series of if-elif-else statements to print out the appropriate letter grade
+        if (gradePercentage >=  90)
         {
             letter = "A";
         }
-        else if (gradePercentage > 80)
+        else if (gradePercentage >= 80)
         {
             letter = "B";
         }
-        else if (gradePercentage > 70)
+        else if (gradePercentage >= 70)
         {
             letter = "C";
         }
-        else if (gradePercentage > 60)
+        else if (gradePercentage >= 60)
         {
             letter = "D";
         }
@@ -31,7 +33,48 @@ class Program
             letter = "F";
         }
 
-        Console.WriteLine($"Your grade is: {letter}");
+
+
+        // Add to your code the ability to include a "+" or "-" next to the letter grade
+        int determineSign = gradePercentage % 10;
+        string sign = "";
+
+        if (determineSign >= 7)
+        {
+            sign = "+";
+        }
+        else if (determineSign >= 3)
+        {
+            sign = " ";
+        }
+        else if (determineSign < 3)
+        {
+            sign = "-";
+        }
+
+        string convertedGrade = letter + sign;
+        string grade = "";
+
+        if (convertedGrade == "A+")
+        {
+            grade = "A";
+        }
+        else if (convertedGrade == "F-")
+        {
+            grade = "F";
+        }
+        else if (convertedGrade == "F+")
+        {
+            grade = "F";
+        }
+        else
+        {
+            grade = convertedGrade;
+        }
+
+
+        // statement to determine if the user passed the course or not
+        Console.WriteLine($"Your grade is: {grade}");
 
         if (gradePercentage > 70)
         {
