7fdd3fb45ebb9dd4a7e46ed535085bb0e6a8c651
Eda Mae Brown <eda_awe92@yahoo.com.ph>
2024-01-11 00:05:42 -0500

    Finished Prep3 challenge

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int magicNumber = 13;
+
+        int guessNumber = -1;
+
+        while (guessNumber != magicNumber) 
+        {
+            Console.Write("Please enter a number between 1 and 20: ");
+            string userInput = Console.ReadLine();
+            guessNumber = int.Parse(userInput);
+
+            if (guessNumber < magicNumber)
+            {
+                Console.WriteLine("Try to guess higher.");
+            }
+            else if (guessNumber > magicNumber)
+            {
+                Console.WriteLine("Try to guess lower.");
+            }
+            else 
+            {
+                Console.WriteLine("You guessed the number!");
+            }
+        }
     }
 }
