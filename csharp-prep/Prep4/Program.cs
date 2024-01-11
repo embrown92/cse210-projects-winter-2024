62cc8b4b09f30e8c0c438725cb8e34209f55c5a6
Eda Mae Brown <eda_awe92@yahoo.com.ph>
2024-01-11 00:47:10 -0500

    Finished prep4 challenge

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,56 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+
+        // Ask user for a list of numbers
+        while (userNumber != 0)
+        {
+            Console.Write("Enter number: ");
+            string userInput = Console.ReadLine();
+            userNumber = int.Parse(userInput);
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        // Find the sum
+        int sum = 0;
+
+        foreach (int number in numbers)
+        {
+         sum += number;   
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        // Find the average
+        float average = ((float)sum) / numbers.Count;
+
+        Console.WriteLine($"The average is: {average}");
+
+        // Find the max number
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                // if this number is greater than the max, we have found the new max!
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
     }
 }
